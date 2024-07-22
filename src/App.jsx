88a207a2edd09139d8a880dc7b9151a6608f29e4
ArtfulLee <aastepsb@yandex.ru>
2024-07-22 12:21:29 +0300

    fix: correct ToDoList component for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/ui/Header/Header";
+import ToDoList from "./components/ui/ToDoList/ToDoList";
 
 export default function App() {
   return (
     <>
       <div className="container mx-auto font-mono font-normal text-stone-900 not-italic">
         <Header />
+        <ToDoList />
       </div>
     </>
   );
