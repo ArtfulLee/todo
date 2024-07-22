075cf0eb661b55ffc16c08c04b62ee3ab929a145
ArtfulLee <aastepsb@yandex.ru>
2024-07-22 15:49:17 +0300

    fix: correct css styles

--- src/components/ui/Header/Header.jsx
+++ src/components/ui/Header/Header.jsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <>
-      <h1 className="font-semibold text-center text-3xl p-4">ToDoApp</h1>
+      <h1 className="font-semibold text-center text-3xl p-4 cursor-default">
+        ToDoApp
+      </h1>
     </>
   );
 };
